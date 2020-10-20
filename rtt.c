6ea8ec0adfa8e857b70974490375c5a2d52cf234
Im not giving my name to a machine! <tk@xarby.com>
2020-10-20 12:48:50 -0700

    minimal demo of pulling timestamps

--- rtt.c
+++ rtt.c
@@ -1,63 +1,3 @@
-/*
- * sniffex.c
- *
- * Sniffer example of TCP/IP packet capture using libpcap.
- * 
- * Version 0.1.1 (2005-07-05)
- * Copyright (c) 2005 The Tcpdump Group
- *
- * This software is intended to be used as a practical example and 
- * demonstration of the libpcap library; available at:
- * http://www.tcpdump.org/
- *
- ****************************************************************************
- *
- * This software is a modification of Tim Carstens' "sniffer.c"
- * demonstration source code, released as follows:
- * 
- * sniffer.c
- * Copyright (c) 2002 Tim Carstens
- * 2002-01-07
- * Demonstration of using libpcap
- * timcarst -at- yahoo -dot- com
- * 
- * "sniffer.c" is distributed under these terms:
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. The name "Tim Carstens" may not be used to endorse or promote
- *    products derived from this software without prior written permission
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * <end of "sniffer.c" terms>
- *
- * This software, "sniffex.c", is a derivative work of "sniffer.c" and is
- * covered by the following terms:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Because this is a derivative work, you must comply with the "sniffer.c"
- *    terms reproduced above.
- * 2. Redistributions of source code must retain the Tcpdump Group copyright
- *    notice at the top of this source file, this list of conditions and the
- *    following disclaimer.
 #include <unistd.h>
 #include <sys/stat.h>
 #include <pcap.h>
@@ -69,7 +9,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 
 /* default snap length (maximum bytes per packet to capture) */
@@ -83,11 +24,6 @@
 
 //----------------------------------------------
 
-FILE *binlog;
-
-long last_time = 0;
-unsigned long bytes_per_port[65536];
-
 /* Ethernet header */
 struct sniff_ethernet {
         u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
@@ -95,157 +31,16 @@ struct sniff_ethernet {
         u_short ether_type;                     /* IP? ARP? RARP? etc */
 };
 
-/* IP header */
-struct sniff_ip {
-        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
-        u_char  ip_tos;                 /* type of service */
-        u_short ip_len;                 /* total length */
-        u_short ip_id;                  /* identification */
-        u_short ip_off;                 /* fragment offset field */
-        #define IP_RF 0x8000            /* reserved fragment flag */
-        #define IP_DF 0x4000            /* dont fragment flag */
-        #define IP_MF 0x2000            /* more fragments flag */
-        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-        u_char  ip_ttl;                 /* time to live */
-        u_char  ip_p;                   /* protocol */
-        u_short ip_sum;                 /* checksum */
-        struct  in_addr ip_src,ip_dst;  /* source and dest address */
-};
-#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
-/* TCP header */
-typedef u_int tcp_seq;
-
-struct sniff_tcp {
-        u_short th_sport;               /* source port */
-        u_short th_dport;               /* destination port */
-        tcp_seq th_seq;                 /* sequence number */
-        tcp_seq th_ack;                 /* acknowledgement number */
-        u_char  th_offx2;               /* data offset, rsvd */
-#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
-        u_char  th_flags;
-        #define TH_FIN  0x01
-        #define TH_SYN  0x02
-        #define TH_RST  0x04
-        #define TH_PUSH 0x08
-        #define TH_ACK  0x10
-        #define TH_URG  0x20
-        #define TH_ECE  0x40
-        #define TH_CWR  0x80
-        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 /* window */
-        u_short th_sum;                 /* checksum */
-        u_short th_urp;                 /* urgent pointer */
-};
-
-//---------
-
-// set up image data 
-	
-typedef struct
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	{
-	unsigned char B;
-	unsigned char G;
-	unsigned char R;
-	} pixel;
-	
-long counts[65535];
-
-long this_second;
-
-///
-
-
-
-
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-
-void print_app_banner(void);
-
-void print_app_usage(void);
-
-void write_image(long *counts, long timestamp);
-
-
-void write_image(long *counts, long timestamp)
-	{
-	char *filename[256];
-	
-	sprintf(filename, "%li.bmp", timestamp);
-	
-	pixel **pixels[1][65535];
-	
-	int fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);  
-	static unsigned char header[54] = {66,77,0,0,0,0,0,0,0,0,54,0,0,0,40,0,0,0,0,0,0,0,0,0,0,0,1,0,24}; //rest is zeroes
-	unsigned int pixelBytesPerRow = width*sizeof(pixel);
-	unsigned int paddingBytesPerRow = (4-(pixelBytesPerRow%4))%4;
-	unsigned int* sizeOfFileEntry = (unsigned int*) &header[2];
-	*sizeOfFileEntry = 54 + (pixelBytesPerRow+paddingBytesPerRow)*height;  
-	unsigned int* widthEntry = (unsigned int*) &header[18];    
-	*widthEntry = width;
-	unsigned int* heightEntry = (unsigned int*) &header[22];    
-	*heightEntry = height;
-	write(fd, header, 54);
-	static unsigned char zeroes[3] = {0,0,0}; //for padding    
-	for (int row = 0; row < height; row++) {
-		write(fd,pixels[row],pixelBytesPerRow);
-		write(fd,zeroes,paddingBytesPerRow);
-	}
-close(fd);
-	
-	
-
-
-
-
-
-/*
- * app name/banner
- */
-void
-print_app_banner(void)
-{
-
-	printf("%s - %s\n", APP_NAME, APP_DESC);
-	printf("%s\n", APP_COPYRIGHT);
-	printf("%s\n", APP_DISCLAIMER);
-	printf("\n");
-
-return;
-}
-
-/*
- * print help text
- */
-void
-print_app_usage(void)
-{
-
-	printf("Usage: %s [interface]\n", APP_NAME);
-	printf("\n");
-	printf("Options:\n");
-	printf("    interface    Listen on <interface> for packets.\n");
-	printf("\n");
-
-return;
-}
-
-
-/*
- * dissect/print packet
- */
-void
-got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
-{
 
 	static int count = 1;                   /* packet counter */
 	
 	/* declare pointers to packet headers */
 	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
-	const struct sniff_ip *ip;              /* The IP header */
-	const struct sniff_tcp *tcp;            /* The TCP header */
-	const struct udphdr *udp;         /* The TCP header */
+	const struct ip *ip;              /* The IP header */
+	const struct tcphdr *tcp;            /* The TCP header */
 	const char *payload;                    /* Packet payload */
 
 	int size_ip;
@@ -259,84 +54,48 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	ethernet = (struct sniff_ethernet*)(packet);
 	
 	/* define/compute ip header offset */
-	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-	size_ip = IP_HL(ip)*4;
+	ip = (struct ip*)(packet + SIZE_ETHERNET);
+	size_ip = ip->ip_hl*4;
 	if (size_ip < 20) {
-		//printf("   * Invalid IP header length: %u bytes\n", size_ip);
 		return;
 	}
 
-	/* print source and destination IP addresses */
-	//printf("       From: %s\n", inet_ntoa(ip->ip_src));
-	//printf("         To: %s\n", inet_ntoa(ip->ip_dst));
-	
-	struct timeval now;
-	gettimeofday(&now, NULL);
+	if(ip->ip_p == IPPROTO_TCP)
+		{
+		//printf("   Protocol: TCP\n");
+		/* define/compute tcp header offset */
+		tcp = (struct tcphdr*)(packet + SIZE_ETHERNET + size_ip);
+		size_tcp = tcp->th_off*4;
 
-	unsigned long sport,dport,len_bytes;
-	len_bytes = ip->ip_len;
+		if (size_tcp < 20) return;
 
-	if(len_bytes < 41)
-		return;
+		if (size_tcp > 24)
+			{
+			uint8_t *kind;
+			uint8_t *len;
 
+			kind = (uint8_t*)(packet + SIZE_ETHERNET + size_ip + sizeof(struct tcphdr));
 
-	//printf("IP Size: %d", len_bytes);
+			while(*kind == TCPOPT_NOP)
+				kind += TCPOLEN_NOP;
 
-	/* determine protocol */	
-	switch(ip->ip_p) {
-		case IPPROTO_TCP:
-			//printf("   Protocol: TCP\n");
-			/* define/compute tcp header offset */
-			tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-			size_tcp = TH_OFF(tcp)*4;
-			if (size_tcp < 20) {
-				//printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-				return;
+			// yes we're skipping any packets that don't have their *first* option
+			// set to timestamps.  whatever, we're going for an average value
+			// and there is plenty of traffic to sample.
+			if(*kind == TCPOPT_TIMESTAMP)
+				{
+				uint32_t *TSVal;
+				uint32_t *TSecr;
+
+				TSVal = (uint32_t*)(kind + 2);
+				TSecr = (uint32_t*)(kind + 2 + 4);
+
+				printf("%d->%d, TSVal: %u, TSecr: %u\n", tcp->th_sport, tcp->th_dport, *TSVal, *TSecr);
+				}
 			}
+		}
 
-			sport = ntohs(tcp->th_sport);
-			dport = ntohs(tcp->th_dport);
-
-			//printf("%d", sizeof(now.tv_usec));
-
-			fwrite(&now.tv_sec, sizeof(now.tv_sec), 1, binlog);
-			fwrite(&sport, sizeof(short), 1, binlog);
-			fwrite(&dport, sizeof(short), 1, binlog);
-			fwrite(&len_bytes, sizeof(short), 1, binlog);
-
-			if(sport < 10000)
-				bytes_per_port[sport] = bytes_per_port[sport] + len_bytes;
-			if(dport < 10000)
-				bytes_per_port[dport] = bytes_per_port[dport] + len_bytes;
-
-			//printf("   Src port: %d\n", ntohs(tcp->th_sport));
-			//printf("   Dst port: %d\n", ntohs(tcp->th_dport));
-			break;
-		case IPPROTO_UDP:
-			//printf("   Protocol: UDP\n");
-			udp = (struct udphdr*)(packet + SIZE_ETHERNET + size_ip);
-
-			sport = ntohs(udp->uh_sport);
-			dport = ntohs(udp->uh_dport);
-
-			fwrite(&now.tv_sec, sizeof(now.tv_sec), 1, binlog);
-			fwrite(&sport, sizeof(short), 1, binlog);
-			fwrite(&dport, sizeof(short), 1, binlog);
-			fwrite(&len_bytes, sizeof(short), 1, binlog);
-
-			if(sport < 10000)
-				bytes_per_port[sport] = bytes_per_port[sport] + len_bytes;
-			if(dport < 10000)
-				bytes_per_port[dport] = bytes_per_port[dport] + len_bytes;
-	
-			//printf("   Src port: %d\n", ntohs(udp->uh_sport));
-			//printf("   Dst port: %d\n", ntohs(udp->uh_dport));
-			return;
-		default:
-			//printf("   Protocol: unknown\n");
-			return;
-	}
-	
+	/* Graphite stuff
 	if(last_time  != now.tv_sec)
 		{
 		//printf("Tick! %d -> %d\n", last_time, now.tv_sec);
@@ -351,13 +110,15 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 		memset(bytes_per_port, 0, sizeof(unsigned long)*65536);
 		last_time = now.tv_sec;
 		}
+	*/
 	
-return;
-}
+	return;
+	}
 
 int main(int argc, char **argv)
-{
+	{
 
+	/*
 	//daemonize!!
         pid_t pid;
         pid = fork();
@@ -377,20 +138,17 @@ int main(int argc, char **argv)
         close(STDERR_FILENO);
 
         /// Daemonized!
+	*/
 
 	char *dev = NULL;			/* capture device name */
 	char errbuf[PCAP_ERRBUF_SIZE];		/* error buffer */
 	pcap_t *handle;				/* packet capture handle */
 
-	char filter_exp[] = "ip";		/* filter expression [3] */
+	char filter_exp[] = "tcp";		/* filter expression [3] */
 	struct bpf_program fp;			/* compiled filter program (expression) */
 	bpf_u_int32 mask;			/* subnet mask */
 	bpf_u_int32 net;			/* ip */
-	int num_packets = -1;			/* number of packets to capture */
-
-	print_app_banner();
-
-	binlog = fopen("stats.bin", "w");
+	int num_packets = 40;			/* number of packets to capture */
 
 	/* check for capture device name on command-line */
 	if (argc == 2) {
@@ -398,7 +156,6 @@ int main(int argc, char **argv)
 	}
 	else if (argc > 2) {
 		fprintf(stderr, "error: unrecognized command-line options\n\n");
-		print_app_usage();
 		exit(EXIT_FAILURE);
 	}
 	else {
@@ -423,9 +180,9 @@ int main(int argc, char **argv)
 	
 	
 	/* print capture info */
-	//printf("Device: %s\n", dev);
-	//printf("Number of packets: %d\n", num_packets);
-	//printf("Filter expression: %s\n", filter_exp);
+	printf("Device: %s\n", dev);
+	printf("Number of packets: %d\n", num_packets);
+	printf("Filter expression: %s\n", filter_exp);
 
 	/* open capture device */
 	handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
@@ -454,8 +211,8 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	char host[] = "173.255.243.159";
-	graphite_init(host, 2003);
+	//char host[] = "173.255.243.159";
+	//graphite_init(host, 2003);
 
 	/* now we can set our callback function */
 	pcap_loop(handle, num_packets, got_packet, NULL);
@@ -463,8 +220,6 @@ int main(int argc, char **argv)
 	/* cleanup */
 	pcap_freecode(&fp);
 	pcap_close(handle);
-
-	memset(bytes_per_port, 0, sizeof(unsigned long) * 65536);
 
 	printf("\nCapture complete.\n");
 
